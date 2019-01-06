fd8cdc886ddbe6fce0798d797596fd9d5b20bf38
Marcus Peck <admin@zombietechinc.com>
2019-01-06 13:21:11 -0500

    homepage loads

--- tool-catalog.py
+++ tool-catalog.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python
+from flask import Flask, render_template, request, redirect, jsonify
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/tools')
+def showHomePage():
+    return render_template('tools-catalog-home.html')
+
+@app.route('/tools/<int:')
+
+
+if __name__ == '__main__':
+    app.secret_key = 'marcus'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
