0e9a7c7b4ffd3657b02d79c388d45d3f0aa2ee5c
Marcus Peck <admin@zombietechinc.com>
2019-01-13 16:01:52 -0500

    delete tool functionality added

--- tool-catalog.py
+++ tool-catalog.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Tool, Brand, User
@@ -13,20 +13,61 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/tools')
+@app.route('/brands')
 def showHomePage():
     brands = session.query(Brand).all()
     print('these are the brands: ')
     print (brands)
     return render_template('tools-catalog-home.html', brands=brands)
 
-@app.route('/tools/<int:brand_id>')
+@app.route('/brands/<int:brand_id>')
 def showBrandPage(brand_id):
-    brand = session.query(Brand).filter_by(id=brand_id)
+    brand = session.query(Brand).filter_by(id=brand_id).one()
     tools = session.query(Tool).filter_by(brand_id=brand_id).all()
+    print ('Brand: ')
+    print (brand.id)
     print ('these are the tools')
     print (tools)
     return render_template('brand-public-page.html', brand=brand, tools=tools)
+
+@app.route('/brands/create-brand', methods=['GET', 'POST'])
+def newBrand():
+    if request.method == 'POST':
+        newBrand = Brand(name = request.form['name'], description = request.form['description'])
+        session.add(newBrand)
+        session.commit()
+        return redirect(url_for('showHomePage'))
+    else:
+        return render_template('newbrand.html')
+
+@app.route('/brands/<int:brand_id>/newtool', methods=['GET', 'POST'])
+def newTool(brand_id):
+    if request.method == 'POST':
+        newTool = Tool(name = request.form['name'], description = request.form['description'], price = request.form['price'],
+            type = request.form['type'], brand_id = brand_id)
+        session.add(newTool)
+        session.commit()
+        return redirect(url_for('showBrandPage', brand_id = brand_id))
+    else:
+        return render_template('newtool.html', brand_id = brand_id)
+
+@app.route('/brands/<int:brand_id>/tools/<int:tool_id>/delete', methods=['GET', 'POST'])
+def deleteTool(brand_id, tool_id):
+    brand = session.query(Brand).filter_by(id=brand_id).one()
+    tool = session.query(Tool).filter_by(id=tool_id).one()
+    if request.method == 'POST':
+        session.delete(tool)
+        session.commit()
+        return redirect(url_for('showBrandPage', brand_id=brand_id))
+    else:
+        return render_template('deletetool.html', brand = brand,  tool = tool)
+
+@app.route('/brands/<int:brand_id>/tools/<int:tool_id>')
+def viewTool(brand_id, tool_id):
+    brand = session.query(Brand).filter_by(id=brand_id).one()
+    tool = session.query(Tool).filter_by(id = tool_id).one()
+    return render_template('tooldetails.html', brand = brand, tool = tool)
+
 
 
 if __name__ == '__main__':
